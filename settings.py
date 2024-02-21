4a91c96cb343fe0c4e4de1b350f535d5388967ea
Sammy Geraci <sammygeraci@gmail.com>
2024-02-21 13:30:38 -0600

    fix participant variables

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ from os import environ
 
 SESSION_CONFIGS = [
     dict(
-        name='LuckEgalitariansVsRelationalEgalitariansInTheLotteryGame',
+        name='SammyCode',
         app_sequence=['DictatorGame', 'IndividualContest', 'RiskTolerance', 'survey', 'ExplainPayoff'],
         num_demo_participants=2,
     ),
@@ -24,6 +24,7 @@ SESSION_CONFIG_DEFAULTS = dict(
     real_world_currency_per_point=1.00, participation_fee=0.00, doc=""
 )
 
+
 PARTICIPANT_FIELDS = ['persistent_id', 'risk_x', 'risk_pi', 'risk_flip', 'final_round_num', 'round_pi', 'round_a', 'round_r', 'round_x', 'round_opponent_x',
                       'round_win', 'round_color']
 SESSION_FIELDS = []
@@ -34,11 +35,10 @@ LANGUAGE_CODE = 'en'
 
 # e.g. EUR, GBP, CNY, JPY
 REAL_WORLD_CURRENCY_CODE = 'USD'
-USE_POINTS = False
+USE_POINTS = True
 
 ADMIN_USERNAME = 'admin'
 # for security, best to set admin password in an environment variable
-ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
 
 DEMO_PAGE_INTRO_HTML = """ """
 
